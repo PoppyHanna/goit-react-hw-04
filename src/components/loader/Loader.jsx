4d694dbfa5f611d72c77429031bd0ac318a709f9
PoppyHanna <142433975+PoppyHanna@users.noreply.github.com>
2024-04-14 21:04:00 -0400

    up5

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,15 +1,4 @@
-// import { Hourglass } from 'react-loader-spinner';
-// render(<Hourglass
-//   visible={true}
-//   height="80"
-//   width="80"
-//   ariaLabel="hourglass-loading"
-//   wrapperStyle={{}}
-//   wrapperClass=""
-//   colors={['#306cce', '#72a1ed']}
-//   />)
 
-// import { Hairball, HairballPreset } from 'react-loader-spinner/dist/beta';
 
 import { Hourglass } from "react-loader-spinner";
 
@@ -31,5 +20,3 @@ const Loader = () => {
 
 export default Loader;
 
-//  <Hairball preset={HairballPreset.dawn} /> 
-// {/* or preset={HairballPreset.sunset} */}
--- src/components/modal/ImageModal.jsx
+++ src/components/modal/ImageModal.jsx
@@ -11,7 +11,7 @@ const ImageModal = ({ isOpen, image, onClose }) => {
       <button onClick={onClose}>Close</button>
       {image && (
         <>
-          <img src={image.urls.full} alt={image.alt_description} /> {/* Змінено urls.regular на urls.full */}
+          <img src={image.urls.full} alt={image.alt_description} /> 
           <p>Author: {image.user.name}</p>
           <p>Likes: {image.likes}</p>
           <p>Description: {image.description || 'No description available'}</p>
